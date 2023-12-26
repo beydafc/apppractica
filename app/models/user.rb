aa0be440571b932887291a3f58bed9bace338cd2
Beyda Fentanes <beyfenta@gmail.com>
2023-12-26 11:05:03 -0600

    Inicio de front para home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Asociaciones:
+
   has_many :restaurants
   has_one_attached :photo
 end
